d14fed0bf94735f8e328e20cd22b8743554369ea
mmangel <mmangel@adents.com>
2016-06-21 10:41:14 +0200

    Add simple-canvas example from lua.vm.js-Samples

---
+++ samples/simple-canvas/simple-canvas.lua
@@ -0,0 +1,57 @@
+-- Examples converted from http://www.alsacreations.com/tuto/lire/1511-introduction-canvas.html
+window.extract()
+
+ui = {
+  canvas = {
+    element,
+    context
+  }
+}
+
+function init()
+  local canvas = document:querySelector(".canvas")
+  -- if canvas == nil or canvas == js.null then
+  --   return
+  -- end
+
+  local context = canvas:getContext("2d")
+
+  ui.canvas.element = canvas
+  ui.canvas.context = context
+
+  draw()
+end
+
+function draw()
+  local ctx = ui.canvas.context;
+
+  -- Let's draw a boat
+
+  ctx:beginPath()
+  ctx:moveTo(150,80)
+  ctx:lineTo(300,230)
+  ctx:lineTo(150,230)
+  ctx:closePath()
+  ctx.fillStyle = "lightblue"
+  ctx:fill()
+
+  ctx:beginPath()
+  ctx:moveTo(50,250)
+  ctx:lineTo(100,300)
+  ctx:lineTo(250,300)
+  ctx:lineTo(300,250)
+  ctx.fillStyle = "peru"
+  ctx.strokeStyle = "sienna"
+  ctx.lineWidth = 5
+  ctx:fill()
+  ctx:stroke()
+
+  ctx:beginPath()
+  ctx:moveTo(140,50)
+  ctx:lineTo(140,250)
+  ctx.lineWidth = 10
+  ctx:stroke()
+
+end
+
+init()
